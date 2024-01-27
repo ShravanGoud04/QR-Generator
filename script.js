049dba1e815230fa7455a416485f02315190c848
ShravanGoud04 <129876799+ShravanGoud04@users.noreply.github.com>
2024-01-27 17:48:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let img = document.querySelector("img");
 let fetching = async (e) => {
   e.preventDefault();
   let response = await fetch(
-    `http://api.qrserver.com/v1/create-qr-code/?size=${size.value}&data=${input.value}`
+    `https://api.qrserver.com/v1/create-qr-code/?size=${size.value}&data=${input.value}`
   );
   console.log(response);
   img.setAttribute("src", response.url);
