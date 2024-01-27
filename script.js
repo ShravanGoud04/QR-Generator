3c53ffc89b67870f5d4a0c06c75dd7672c2e2bcd
ShravanGoud04 <shravangoud2119@gmail.com>
2024-01-27 17:42:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let form = document.querySelector("form");
+
+let size = document.querySelector("#size");
+let input = document.querySelector("#input");
+let img = document.querySelector("img");
+
+let fetching = async (e) => {
+  e.preventDefault();
+  let response = await fetch(
+    `http://api.qrserver.com/v1/create-qr-code/?size=${size.value}&data=${input.value}`
+  );
+  console.log(response);
+  img.setAttribute("src", response.url);
+  form.reset();
+};
+
+form.addEventListener("submit", fetching);
